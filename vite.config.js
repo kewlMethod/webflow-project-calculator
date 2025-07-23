81fd754982fc1ea44d32dd3c752c4af738ccb818
kewlMethod <159096744+kewlMethod@users.noreply.github.com>
2025-07-23 17:06:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
 })
-
-// ================================================================
-
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-// ================================================================
-
-// postcss.config.js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-
-// ================================================================
-
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
-// ================================================================
-
-// src/index.css
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
